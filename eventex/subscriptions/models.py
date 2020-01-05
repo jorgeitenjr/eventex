bcdd3ccb5f09bdec529955c9b577bf64fe4da195
jorgeiten <jorgeiten@gmail.com>
2020-01-05 18:31:14 -0300

    Email ou telefone opcionais

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -12,8 +12,8 @@ def validate_cpf(value):
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
     cpf = models.CharField('cpf', max_length=11, validators=[validate_cpf])
-    email = models.EmailField('e-mail')
-    phone = models.CharField('telefone', max_length=20)
+    email = models.EmailField('Email', blank=True)
+    phone = models.CharField('telefone', max_length=20, blank=True)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     paid = models.BooleanField('pago', default=False)
 
--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -74,6 +74,13 @@ class SubscriptionsNewPostInvalid(TestCase):
         self.assertFalse(Subscription.objects.exists())
 
 
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Henrique Bastos', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
+
+
 class SubscribeSuccessMessage(TestCase):
     def test_message(self):
         data = dict(name='Henrique Bastos', cpf='12345678901', email='henrique@bastos.net', phone='21-99618-6180')
